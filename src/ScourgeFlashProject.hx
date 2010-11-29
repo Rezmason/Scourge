eecd15ece13c52c9610f42e250a1e8efaf291478
Rezmason <jeremysachs@rezmason.net>
2010-11-29 14:57:03 +0000

    Giving better names to things, mostly.

--- src/ScourgeFlashProject.hx
+++ src/ScourgeFlashProject.hx
@@ -7,7 +7,7 @@ import net.rezmason.scourge.Game;
 class ScourgeFlashProject extends Sprite {
 	public static function main():Void { 
 		var defaultGrid:String = Lib.current.loaderInfo.parameters.defaultGrid;
-		if (defaultGrid == null) defaultGrid = "";
+		if (defaultGrid == null) defaultGrid = "-1";
 		var board:Board = new Board(new Game(defaultGrid.split(",")), Lib.current); 
 		var splash:Array<String> = [
 			" %%%%    %%%      %%%    %   %    %%%%      %%%%    %%%", 
