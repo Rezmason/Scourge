0054b3a3f520790d6d05cf7b1fa2bcf6ff26e98a
Rezmason <jeremysachs@rezmason.net>
2011-01-06 20:17:35 +0000

    Replacing EaselJS stuff with new version 0.2.

--- lib/EaselJS/hxclasses/com/gskinner/display/Bitmap.hx
+++
@@ -1,9 +0,0 @@
-package com.gskinner.display;
-
-extern class Bitmap extends DisplayObject {
-	var image:Dynamic;
-	function new(image:Dynamic):Void;
-	override function draw(ctx:Dynamic, ?ignoreCache:Bool):Bool;
-	function cache():Void;
-	function uncache():Void;
-}
