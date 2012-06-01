1b3c43403f4a7272c5b7344027cc568feb45aa28
Rezmason <jeremysachs@rezmason.net>
2012-06-01 12:52:07 -0700

    Rearchitecting and starting off with munit.

--- src/Scourge.hx
+++ src/Scourge.hx
@@ -2,58 +2,28 @@ import flash.display.Sprite;
 import flash.events.Event;
 import flash.Lib;
 
-#if proto
-	import net.rezmason.scourge.proto.view.Board;
-	import net.rezmason.scourge.proto.model.Game;
-	import net.rezmason.scourge.proto.swipe.SwipeView;
-#else
-
-#end
-import net.rezmason.scourge.Common;
-
 class Scourge {
 
-	public static function main():Void {
-		Lib.current.loaderInfo.addEventListener(Event.INIT, init);
-	}
+    public static function main():Void {
+        Lib.current.loaderInfo.addEventListener(Event.INIT, init);
+    }
 
-	public static function init(?event:Event):Void {
-		Lib.current.loaderInfo.removeEventListener("init", init);
+    public static function init(?event:Event):Void {
+        Lib.current.loaderInfo.removeEventListener("init", init);
 
-		var params:Dynamic = Lib.current.loaderInfo.parameters;
+        var params:Dynamic = Lib.current.loaderInfo.parameters;
 
-		var defaultGrid:String = params.defaultGrid;
-		var numPlayers:Int = Std.parseInt(params.numPlayers);
-		var circular:Bool = (params.circular == "true" || params.circular == "1");
-		var duration:Float = params.duration;
 
-		if (defaultGrid == null) defaultGrid = "-1";
-		if (numPlayers == 0) numPlayers = 4;
-		if (Math.isNaN(duration)) duration = 0;
 
-		var options:GameOptions = {
-			numPlayers:numPlayers,
-			circular:circular,
-			duration:duration,
-		};
+        var splash:Array<String> = [
+            " %%%%    %%%      %%%    %   %    %%%%      %%%%    %%% ",
+            "%       %   %    %   %   %   %    %   %    %   %   %   %",
+            "%%%%%   %        %   %   %   %    %%%%      %%%%   %%%%%",
+            "    %   %   %    %   %   %   %    %   %        %   %    ",
+            "%%%%     %%%      %%%     %%%     %   %    %%%%     %%%%",
+            "Single-Celled Organisms Undergo Rapid Growth Enhancement",
+        ];
 
-		#if proto
-			var game:Game = new Game(defaultGrid.split(","));
-			var board:Board = new Board(game, Lib.current, options);
-			//var view:SwipeView = new SwipeView(game, Lib.current, options);
-		#else
-
-		#end
-
-		var splash:Array<String> = [
-			" %%%%    %%%      %%%    %   %    %%%%      %%%%    %%% ",
-			"%       %   %    %   %   %   %    %   %    %   %   %   %",
-			"%%%%%   %        %   %   %   %    %%%%      %%%%   %%%%%",
-			"    %   %   %    %   %   %   %    %   %        %   %    ",
-			"%%%%     %%%      %%%     %%%     %   %    %%%%     %%%%",
-			"Single-Celled Organisms Undergo Rapid Growth Enhancement",
-		];
-
-		//Lib.trace(splash.join("\n"));
-	}
+        Lib.trace(splash.join("\n"));
+    }
 }
