e2cab0ca2b7c6a072a03661127b7ee8f44af0ca0
Rezmason <jeremysachs@rezmason.net>
2012-11-22 20:14:15 -0500

    Moving the aspect and rule macros to their respective classes.

--- src/net/rezmason/scourge/model/Aspect.hx
+++ src/net/rezmason/scourge/model/Aspect.hx
@@ -1,9 +1,61 @@
 package net.rezmason.scourge.model;
 
-#if !macro @:autoBuild(net.rezmason.scourge.model.AspectBuilder.build()) #end class Aspect {
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+import net.rezmason.scourge.model.ModelTypes;
+#end
+
+#if !macro @:autoBuild(net.rezmason.scourge.model.Aspect.build()) #end class Aspect {
     private static var ids:Int = 0;
     public inline static var TRUE:Int = 1;
     public inline static var FALSE:Int = 0;
     public inline static var NULL:Int = -1;
-}
 
+    @:macro public static function build():Array<Field> {
+
+        var msg:String = "Building " + Context.getLocalClass().get().name + "  ";
+
+        var pos:Position = Context.currentPos();
+        var fields:Array<Field> = Context.getBuildFields();
+
+        for (field in fields) {
+
+            for (metaTag in field.meta) {
+                if (metaTag.name == "aspect") {
+
+                    var aspectExpr:Expr = metaTag.params[0];
+
+                    // Turn some literal values into Aspect consts
+                    switch (aspectExpr.expr) {
+                        case EConst(c):
+                            switch (c) {
+                                case CIdent(s):
+                                    aspectExpr = Context.parse("Aspect." + s.toUpperCase(), field.pos);
+                                default:
+                            }
+                        default:
+                    }
+
+                    metaTag.params = [];
+                    var expr:Expr = macro {id:Aspect.ids++, initialValue:$aspectExpr};
+
+                    field.access = [AStatic, APublic];
+                    field.kind = FVar(null, {pos:field.pos, expr:expr.expr});
+
+                    msg += metaTag.name.charAt(0);
+
+                    break;
+                }
+            }
+        }
+
+        msg += "\n";
+
+        #if SCOURGE_VERBOSE
+            neko.Lib.print(msg);
+        #end
+
+        return fields;
+    }
+}
--- src/net/rezmason/scourge/model/Rule.hx
+++ src/net/rezmason/scourge/model/Rule.hx
@@ -1,10 +1,17 @@
 package net.rezmason.scourge.model;
 
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+using Lambda;
+#end
+
 import net.rezmason.scourge.model.ModelTypes;
 
 using net.rezmason.utils.Pointers;
 
-#if !macro @:autoBuild(net.rezmason.scourge.model.RuleBuilder.build()) #end class Rule {
+#if !macro @:autoBuild(net.rezmason.scourge.model.Rule.build()) #end class Rule {
 
     var state:State;
     var plan:StatePlan;
@@ -89,5 +96,79 @@ using net.rezmason.utils.Pointers;
     inline function playerPtr(prop:AspectProperty):AspectPtr { return plan.playerAspectLookup[prop.id]; }
     inline function nodePtr(prop:AspectProperty):AspectPtr { return plan.nodeAspectLookup[prop.id]; }
     inline function extraPtr(prop:AspectProperty):AspectPtr { return extraAspectLookup[prop.id]; }
+
+    #if macro
+    private static var lkpSources:Hash<String> = {
+        var hash:Hash<String> = new Hash<String>();
+        hash.set("state",  "plan");
+        hash.set("player", "plan");
+        hash.set("node",   "plan");
+        hash.set("extra",  "this");
+        hash;
+    }
+
+    private static var restrictedFields:Array<String> = [ "__initReqs", "__initPointers", ];
+    #end
+
+    @:macro public static function build():Array<Field> {
+
+        var msg:String = "Building " + Context.getLocalClass().get().name + "  ";
+
+        var pos:Position = Context.currentPos();
+        var fields:Array<Field> = Context.getBuildFields();
+
+        var reqExpressions:Array<Expr> = [];
+        var ptrExpressions:Array<Expr> = [];
+
+        for (field in fields) {
+
+            if (restrictedFields.has(field.name)) {
+                throw new Error("Rules cannot manually override the function " + field.name, field.pos);
+            }
+
+            for (metaTag in field.meta) {
+                if (lkpSources.exists(metaTag.name)) {
+
+                    var aspectExpr:Expr = metaTag.params[0];
+                    metaTag.params = [];
+
+                    field.access.remove(AStatic);
+
+                    var reqs:Expr = Context.parse(metaTag.name + "AspectRequirements", pos);
+                    var ptr:Expr = Context.parse(field.name, pos);
+                    var lkp:Expr = Context.parse(lkpSources.get(metaTag.name) + "." + metaTag.name + "AspectLookup", pos);
+
+                    reqExpressions.push( macro $reqs.push($aspectExpr) );
+                    ptrExpressions.push( macro $ptr = $lkp[$aspectExpr.id] );
+
+                    //neko.Lib.println(macro :net.rezmason.scourge.model.ModelTypes.AspectPtr);
+
+                    field.kind = FVar(macro :net.rezmason.scourge.model.ModelTypes.AspectPtr, null);
+
+                    msg += metaTag.name.charAt(0);
+
+                    break;
+                }
+            }
+        }
+
+        msg += "\n";
+
+        fields.push(overrider("__initReqs", reqExpressions, pos));
+        fields.push(overrider("__initPtrs", ptrExpressions, pos));
+
+        #if SCOURGE_VERBOSE
+            neko.Lib.print(msg);
+        #end
+
+        return fields;
+    }
+
+    #if macro
+    private inline static function overrider(name:String, expressions:Array<Expr>, pos:Position):Field {
+        var func:Function = {params:[], args:[], ret:null, expr:{pos:pos, expr:EBlock(expressions)}};
+        return { name:name, access:[APrivate, AOverride], kind:FFun(func), pos:pos };
+    }
+    #end
 }
 
