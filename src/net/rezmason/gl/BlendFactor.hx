b5a717aad3bd1830b39414237b1509a607e987e3
Rezmason <jeremysachs@rezmason.net>
2014-06-18 01:47:39 -0700

    Oops! Most of BlendFactor’s constants, though unused, didn’t match the constants on Flash’s Context3DBlendFactor.

--- src/net/rezmason/gl/BlendFactor.hx
+++ src/net/rezmason/gl/BlendFactor.hx
@@ -10,16 +10,16 @@ package net.rezmason.gl;
         @:from static public inline function fromInt(val:Int):BlendFactor return new BlendFactor(val);
         @:to public inline function toInt():Int return cast this;
 
-        public static inline var ZERO                  :BlendFactor = GL.ZERO;
-        public static inline var ONE                   :BlendFactor = GL.ONE;
-        public static inline var SRC_COLOR             :BlendFactor = GL.SRC_COLOR;
-        public static inline var ONE_MINUS_SRC_COLOR   :BlendFactor = GL.ONE_MINUS_SRC_COLOR;
-        public static inline var SRC_ALPHA             :BlendFactor = GL.SRC_ALPHA;
-        public static inline var ONE_MINUS_SRC_ALPHA   :BlendFactor = GL.ONE_MINUS_SRC_ALPHA;
-        public static inline var DST_ALPHA             :BlendFactor = GL.DST_ALPHA;
-        public static inline var ONE_MINUS_DST_ALPHA   :BlendFactor = GL.ONE_MINUS_DST_ALPHA;
-        public static inline var DST_COLOR             :BlendFactor = GL.DST_COLOR;
-        public static inline var ONE_MINUS_DST_COLOR   :BlendFactor = GL.ONE_MINUS_DST_COLOR;
-        public static inline var SRC_ALPHA_SATURATE    :BlendFactor = GL.SRC_ALPHA_SATURATE;
+        public static inline var ZERO                          :BlendFactor = GL.ZERO;
+        public static inline var ONE                           :BlendFactor = GL.ONE;
+        public static inline var SOURCE_COLOR                  :BlendFactor = GL.SRC_COLOR;
+        public static inline var ONE_MINUS_SOURCE_COLOR        :BlendFactor = GL.ONE_MINUS_SRC_COLOR;
+        public static inline var SOURCE_ALPHA                  :BlendFactor = GL.SRC_ALPHA;
+        public static inline var ONE_MINUS_SOURCE_ALPHA        :BlendFactor = GL.ONE_MINUS_SRC_ALPHA;
+        public static inline var DESTINATION_ALPHA             :BlendFactor = GL.DST_ALPHA;
+        public static inline var ONE_MINUS_DESTINATION_ALPHA   :BlendFactor = GL.ONE_MINUS_DST_ALPHA;
+        public static inline var DESTINATION_COLOR             :BlendFactor = GL.DST_COLOR;
+        public static inline var ONE_MINUS_DESTINATION_COLOR   :BlendFactor = GL.ONE_MINUS_DST_COLOR;
+        public static inline var SOURCE_ALPHA_SATURATE         :BlendFactor = GL.SRC_ALPHA_SATURATE;
     }
 #end
