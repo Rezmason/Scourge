98adba049e564a149b0166a0cbeb09cfcba6ace4
Rezmason <jeremysachs@rezmason.net>
2015-01-09 07:59:26 -0800

    Removing unused pointers from FreshnessAspect.

--- src/net/rezmason/scourge/model/aspects/FreshnessAspect.hx
+++ src/net/rezmason/scourge/model/aspects/FreshnessAspect.hx
@@ -5,8 +5,4 @@ import net.rezmason.ropes.Aspect;
 class FreshnessAspect extends Aspect {
     @aspect(0) var FRESHNESS;
     @aspect(0) var MAX_FRESHNESS;
-
-    @aspect(null) var FRESH_FIRST;
-    @aspect(null) var FRESH_NEXT;
-    @aspect(null) var FRESH_PREV;
 }
