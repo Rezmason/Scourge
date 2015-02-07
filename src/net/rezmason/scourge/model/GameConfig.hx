3ff13f62b04d75d434b0461386db58dd52b827f5
Rezmason <jeremysachs@rezmason.net>
2015-02-07 09:41:15 -0800

    Siphon's getting annoying, actually.

--- src/net/rezmason/scourge/model/GameConfig.hx
+++ src/net/rezmason/scourge/model/GameConfig.hx
@@ -5,10 +5,11 @@ class GameConfig {
     public var params(default, null):Map<String, Dynamic> = new Map();
     public var configs(default, null):Map<String, Config<Dynamic>> = new Map();
 
-    public function new(defs:Map<String, Class<Config<Dynamic>>>) {
-        for (key in defs.keys()) {
-            configs[key] = Type.createInstance(defs[key], []);
-            params[key] = configs[key].defaultParams();
+    public function new(defs:Array<Class<Config<Dynamic>>>) {
+        for (def in defs) {
+            var config:Config<Dynamic> = Type.createInstance(def, []);
+            configs[config.id()] = config;
+            params[config.id()] = config.defaultParams();
         }
     }
 }
--- src/net/rezmason/scourge/model/ScourgeConfig.hx
+++ src/net/rezmason/scourge/model/ScourgeConfig.hx
@@ -1,12 +1,32 @@
 package net.rezmason.scourge.model;
 
-import net.rezmason.utils.Siphon;
+import net.rezmason.scourge.model.bite.*;
+import net.rezmason.scourge.model.body.*;
+import net.rezmason.scourge.model.build.*;
+import net.rezmason.scourge.model.meta.*;
+import net.rezmason.scourge.model.piece.*;
 
 class ScourgeConfig extends GameConfig {
-    
-    static var configDefs(default, null):Map<String, Class<Config<Dynamic>>> = cast Siphon.getDefs(
-        'net.rezmason.scourge.model', 'src', true, "Config$"
-    );
 
-    public function new() super(configDefs);
+    public var biteParams(get, null):BiteParams;
+    public var bodyParams(get, null):BodyParams;
+    public var buildParams(get, null):BuildParams;
+    public var metaParams(get, null):MetaParams;
+    public var pieceParams(get, null):PieceParams;
+    
+    public function new() {
+        super([
+            BiteConfig,
+            BodyConfig,
+            BuildConfig,
+            MetaConfig,
+            PieceConfig,
+        ]);
+    }
+
+    inline function get_biteParams():BiteParams return params['bite'];
+    inline function get_bodyParams():BodyParams return params['body'];
+    inline function get_buildParams():BuildParams return params['build'];
+    inline function get_metaParams():MetaParams return params['meta'];
+    inline function get_pieceParams():PieceParams return params['piece'];
 }
