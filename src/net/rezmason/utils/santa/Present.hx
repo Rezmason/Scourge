87ed7dada18ae6e05c81757970d2f153509ccc7c
Rezmason <jeremysachs@rezmason.net>
2015-04-21 20:25:39 -0700

    Present now accepts an ID, and Santa can register presents with a class and ID.

--- src/net/rezmason/utils/santa/Present.hx
+++ src/net/rezmason/utils/santa/Present.hx
@@ -1,6 +1,6 @@
 package net.rezmason.utils.santa;
 
 abstract Present<T>(T) {
-    public inline function new(clazz:Class<T>) this = Santa.askFor(clazz);
+    public inline function new(clazz:Class<T>, ?id:String) this = Santa.askFor(clazz, id);
     @:to public inline function toT():T return this;
 }
--- src/net/rezmason/utils/santa/Santa.hx
+++ src/net/rezmason/utils/santa/Santa.hx
@@ -12,6 +12,7 @@ enum InjectionType<T> {
 class Santa {
 
     static var classMap:ObjectMap<Dynamic, InjectionType<Dynamic>> = new ObjectMap();
+    static var idClassMap:ObjectMap<Dynamic, Map<String, InjectionType<Dynamic>>> = new ObjectMap();
 
     public inline static function mapToClass<T>(clazz:Class<T>, present:InjectionType<T>):Bool {
         var replacing:Bool = classMap.exists(clazz);
@@ -19,10 +20,21 @@ class Santa {
         return replacing;
     }
 
+    public inline static function mapToID<T>(clazz:Class<T>, id:String, present:InjectionType<T>):Bool {
+        if (!idClassMap.exists(clazz)) idClassMap.set(clazz, new Map());
+        var idMap = idClassMap.get(clazz);
+        var replacing:Bool = idMap.exists(id);
+        idMap.set(id, present);
+        return replacing;
+    }
+
     @:allow(net.rezmason.utils.santa)
-    inline static function askFor<T>(clazz:Class<T>):T {
+    inline static function askFor<T>(clazz:Class<T>, ?id:String):T {
         var result:T = null;
-        switch (classMap.get(clazz)) {
+        var type = null;
+        if (id == null) type = classMap.get(clazz);
+        else if (idClassMap.exists(clazz)) type = idClassMap.get(clazz).get(id);
+        switch (type) {
             case null: throw 'Santa can\'t give you a ${clazz}';
             case Instance(classToInstantiate, args): result = Type.createInstance(classToInstantiate, args);
             case Singleton(instance): result = instance;
