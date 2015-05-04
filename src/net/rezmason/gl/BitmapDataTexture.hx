5f64012035f84fac73bf4b0d82d5af673e6b6792
Rezmason <jeremysachs@rezmason.net>
2015-05-04 08:09:45 -0700

    Fixing compiler errors in GL layer.

--- src/net/rezmason/gl/BitmapDataTexture.hx
+++ src/net/rezmason/gl/BitmapDataTexture.hx
@@ -10,9 +10,7 @@ import net.rezmason.gl.GLTypes;
 class BitmapDataTexture extends Texture {
 
     var bitmapData:BitmapData;
-    #if flash
-        var nativeTexture:NativeTexture;
-    #end
+    var nativeTexture:NativeTexture;
 
     public function new(bitmapData:BitmapData):Void {
         super();
@@ -25,15 +23,25 @@ class BitmapDataTexture extends Texture {
             var recTex = context.createRectangleTexture(bitmapData.width, bitmapData.height, cast "rgbaHalfFloat", false); // Context3DTextureFormat.RGBA_HALF_FLOAT
             recTex.uploadFromBitmapData(bitmapData);
             nativeTexture = recTex;
+        #else
+            nativeTexture = GL.createTexture();
+            GL.bindTexture (GL.TEXTURE_2D, nativeTexture);
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
+            
+            var pixelData = @:privateAccess (bitmapData.__image).data;
+            GL.texImage2D (GL.TEXTURE_2D, 0, GL.RGBA, bitmapData.width, bitmapData.height, 0, GL.RGBA, GL.UNSIGNED_BYTE, pixelData);
+            
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+            GL.bindTexture (GL.TEXTURE_2D, null);
         #end
     }
 
     override function disconnectFromContext():Void {
         super.disconnectFromContext();
-        #if flash
-            nativeTexture.dispose();
-            nativeTexture = null;
-        #end
+        #if flash nativeTexture.dispose(); #end
+        nativeTexture = null;
     }
 
     override function setAtProgLocation(prog:NativeProgram, location:UniformLocation, index:Int):Void {
@@ -42,12 +50,7 @@ class BitmapDataTexture extends Texture {
                 prog.setTextureAt(location, nativeTexture);
             #else
                 GL.activeTexture(GL.TEXTURE0 + index);
-                GL.bindBitmapDataTexture(bitmapData);
-                GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
-                GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
-                GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
-                GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
-                GL.uniform1i(location, index);
+                GL.bindTexture (GL.TEXTURE_2D, nativeTexture);
             #end
         }
     }
--- src/net/rezmason/gl/Data.hx
+++ src/net/rezmason/gl/Data.hx
@@ -1,6 +1,6 @@
 package net.rezmason.gl;
 
-typedef ReadbackData = #if js openfl.utils.UInt8Array #else flash.utils.ByteArray #end ;
+typedef ReadbackData = #if js openfl.utils.UInt8Array #elseif flash flash.utils.ByteArray #else openfl.utils.UInt8Array #end ;
 
 typedef IndexArray = #if flash flash.Vector<UInt> #else openfl.utils.Int16Array #end ;
-typedef VertexArray = #if flash flash.Vector<Float> #else openfl.utils.Float32Array #end;
+typedef VertexArray = #if flash flash.Vector<Float> #else openfl.utils.Float32Array #end ;
