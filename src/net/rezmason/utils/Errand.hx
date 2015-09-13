c0a8326fad4128f5c9737625d17f55bf9d8f6738
Rezmason <jeremysachs@rezmason.net>
2015-09-13 16:00:22 -0700

    Fixing compiler error.

--- src/net/rezmason/utils/Errand.hx
+++ src/net/rezmason/utils/Errand.hx
@@ -1,5 +1,7 @@
 package net.rezmason.utils;
 
+import haxe.Constraints.Function;
+
 class Errand<T:(Function)> {
     public var onComplete(default, null):Zig<T> = new Zig();
     public function run():Void throw 'Override';
