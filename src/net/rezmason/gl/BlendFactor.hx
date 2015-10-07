6a7609024331e16a1884f47ccdc0858d87d838c1
Rezmason <jeremysachs@rezmason.net>
2015-10-07 08:36:33 -0700

    BlendFactor should be an abstract enum.

--- src/net/rezmason/gl/BlendFactor.hx
+++ src/net/rezmason/gl/BlendFactor.hx
@@ -4,22 +4,17 @@ package net.rezmason.gl;
     typedef BlendFactor = flash.display3D.Context3DBlendFactor;
 #else
     import lime.graphics.opengl.GL;
-
-    abstract BlendFactor(Int) {
-        inline function new(val:Int):Void this = val;
-        @:from static public inline function fromInt(val:Int):BlendFactor return new BlendFactor(val);
-        @:to public inline function toInt():Int return cast this;
-
-        public static inline var ZERO                          :BlendFactor = GL.ZERO;
-        public static inline var ONE                           :BlendFactor = GL.ONE;
-        public static inline var SOURCE_COLOR                  :BlendFactor = GL.SRC_COLOR;
-        public static inline var ONE_MINUS_SOURCE_COLOR        :BlendFactor = GL.ONE_MINUS_SRC_COLOR;
-        public static inline var SOURCE_ALPHA                  :BlendFactor = GL.SRC_ALPHA;
-        public static inline var ONE_MINUS_SOURCE_ALPHA        :BlendFactor = GL.ONE_MINUS_SRC_ALPHA;
-        public static inline var DESTINATION_ALPHA             :BlendFactor = GL.DST_ALPHA;
-        public static inline var ONE_MINUS_DESTINATION_ALPHA   :BlendFactor = GL.ONE_MINUS_DST_ALPHA;
-        public static inline var DESTINATION_COLOR             :BlendFactor = GL.DST_COLOR;
-        public static inline var ONE_MINUS_DESTINATION_COLOR   :BlendFactor = GL.ONE_MINUS_DST_COLOR;
-        public static inline var SOURCE_ALPHA_SATURATE         :BlendFactor = GL.SRC_ALPHA_SATURATE;
+    @:enum abstract BlendFactor(Int) to Int {
+        var ZERO                        = GL.ZERO;
+        var ONE                         = GL.ONE;
+        var SOURCE_COLOR                = GL.SRC_COLOR;
+        var ONE_MINUS_SOURCE_COLOR      = GL.ONE_MINUS_SRC_COLOR;
+        var SOURCE_ALPHA                = GL.SRC_ALPHA;
+        var ONE_MINUS_SOURCE_ALPHA      = GL.ONE_MINUS_SRC_ALPHA;
+        var DESTINATION_ALPHA           = GL.DST_ALPHA;
+        var ONE_MINUS_DESTINATION_ALPHA = GL.ONE_MINUS_DST_ALPHA;
+        var DESTINATION_COLOR           = GL.DST_COLOR;
+        var ONE_MINUS_DESTINATION_COLOR = GL.ONE_MINUS_DST_COLOR;
+        var SOURCE_ALPHA_SATURATE       = GL.SRC_ALPHA_SATURATE;
     }
 #end
