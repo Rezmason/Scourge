5136d19ec24f40cd2e1718a7a04ab3a334222083
Rezmason <jeremysachs@rezmason.net>
2016-02-02 00:06:40 -0800

    Got half float textures working in Chrome, but no where else.
    You're too good for this world, OES_texture_half_float.

--- src/net/rezmason/gl/DataType.hx
+++ src/net/rezmason/gl/DataType.hx
@@ -3,5 +3,6 @@ package net.rezmason.gl;
 import lime.graphics.opengl.GL;
 @:enum abstract DataType(Int) to Int {
     var FLOAT = GL.FLOAT;
+    var HALF_FLOAT = 0x8D61;
     var UNSIGNED_BYTE = GL.UNSIGNED_BYTE;
 }
--- src/net/rezmason/gl/HalfFloatTexture.hx
+++ src/net/rezmason/gl/HalfFloatTexture.hx
@@ -2,17 +2,31 @@ package net.rezmason.gl;
 
 import haxe.io.Bytes;
 import haxe.io.BytesOutput;
+import lime.graphics.opengl.GL;
 import lime.utils.Float32Array;
+import lime.utils.UInt16Array;
 import net.rezmason.utils.HalfFloatUtil;
 
 class HalfFloatTexture extends DataTexture {
     public function new(width:Int, height:Int, bytes:Bytes, ?singleChannel:Bool):Void {
+        // Float conversion init
+        //*
         var input = new haxe.io.BytesInput(bytes);
         var output = new BytesOutput();
-        for (ike in 0...width * height * 4) {
-            var halfFloat:UInt = (!singleChannel || ike % 4 == 0) ? input.readUInt16() : 0;
-            output.writeFloat(HalfFloatUtil.halfFloatToFloat(halfFloat));
-        }
-        super(width, height, RGBA, FLOAT, Float32Array.fromBytes(output.getBytes()));
+        while (input.position < input.length) output.writeFloat(HalfFloatUtil.halfFloatToFloat(input.readUInt16()));
+        super(width, height, singleChannel ? LUMINANCE : RGBA, FLOAT, Float32Array.fromBytes(output.getBytes()));
+        /**/
+        
+
+        // Half float init
+        /*
+        GL.getExtension('OES_texture_half_float');
+        GL.getExtension('OES_texture_half_float_linear');
+
+        var unpackAlignment = GL.getParameter(GL.UNPACK_ALIGNMENT);
+        GL.pixelStorei(GL.UNPACK_ALIGNMENT, singleChannel ? 1 : 4);
+        super(width, height, singleChannel ? LUMINANCE : RGBA, HALF_FLOAT, UInt16Array.fromBytes(bytes));
+        GL.pixelStorei(GL.UNPACK_ALIGNMENT, unpackAlignment);
+        /**/
     }
 }
